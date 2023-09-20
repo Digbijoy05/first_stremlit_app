217f8e03329fdfd728515053783941df686d1720
Digbijoy05 <145515115+Digbijoy05@users.noreply.github.com>
2023-09-20 09:37:10 -0600

    Update streamlit_app.py
    stop cmd

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
+#stop running from here
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
